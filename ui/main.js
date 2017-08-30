c455a49a88fce052a1451f99a60a97340edf6b0c
kalyans12 <kalyansiva12@gmail.com>
2017-08-30 11:26:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the content of the element with id main text 
 document.getElementById('main-text').innerHTML ="My Life My Rules God Helps+':)'";
 
+//Move the image
+var img = document.getElementById('img');
+img.onclick=function(){
+    img.style.marginleft='100px';
+};
