b729c6c3d22bd774c58c14c9c613fd284663cc44
kalyans12 <kalyansiva12@gmail.com>
2017-08-30 11:36:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ document.getElementById('main-text').innerHTML ="My Life My Rules God Helps+':)'
 
 //Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft +10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval = setInterval(moveRight,100);
 };
