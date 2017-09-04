b0e7adc5eb46cb6acd09ca4f10b6a822087cac79
kalyans12 <kalyansiva12@gmail.com>
2017-09-04 10:14:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.post('/create-user',function(req,res){
          if(err){
             res.status(500).send(err.toString());
         }else{
-            res.send("Usee Succesfully created"+userName);
+            result.send("User Succesfully created"+userName);
         }
         });
 });
